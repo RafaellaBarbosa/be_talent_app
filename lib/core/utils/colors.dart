736cef6aec3792a7b07503b825007e43905135a5
Rafaella Barbosa <rafaellacsta@hotmail.com>
2024-11-20 14:04:07 -0300

    first commit

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color blueprimary = Color(0xff0500ff);
+  static const Color blue10 = Color(0xffedeffb);
+
+  static const Color black = Color(0xff1c1c1c);
+  static const Color gray20 = Color(0xff9e9e9e);
+  static const Color gray10 = Color(0xffdfdfdf);
+  static const Color white = Color(0xffffffff);
+}
