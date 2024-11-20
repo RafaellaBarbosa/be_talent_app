b8e2fb6febf2c45a6898743701fb302d368e0517
Rafaella Barbosa <rafaellacsta@hotmail.com>
2024-11-20 15:19:43 -0300

    style: configurada fonte padrão para o aplicativo

---
+++ lib/core/utils/text_styles.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static String fontFamily = 'Helvetica';
+
+  TextStyle heading1Medium = TextStyle(
+    fontSize: 20,
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w500,
+  );
+
+  TextStyle heading2Medium = TextStyle(
+    fontSize: 16,
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w500,
+  );
+
+  TextStyle heading3 = TextStyle(
+    fontSize: 16,
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w400,
+  );
+}
