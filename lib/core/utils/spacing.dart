7002c3117fcff410e29a042ceff67368546fe2c6
Rafaella Barbosa <rafaellacsta@hotmail.com>
2024-11-20 15:20:25 -0300

    style: configurado espaçamento para melhorar legibilidade

---
+++ lib/core/utils/spacing.dart
@@ -0,0 +1,13 @@
+class Spacing {
+  static const double medium80 = 80;
+  static const double medium60 = 60;
+  static const double medium40 = 40;
+  static const double medium32 = 32;
+
+  static const double regular28 = 28;
+  static const double regular20 = 20;
+  static const double regular16 = 16;
+
+  static const double little08 = 8;
+  static const double little04 = 4;
+}
